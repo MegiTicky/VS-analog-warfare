52456fed334bba193e8594e9eaf96b9ef27e96dd
MegiRicky <lauyathong3518@gmail.com>
2026-08-19 19:11:38 +0800

    Fix Ender transmitter password length

--- src/main/java/com/erika/vsanalogwarfare/mixin/client/EnderTransmitterScreenMixin.java
+++ src/main/java/com/erika/vsanalogwarfare/mixin/client/EnderTransmitterScreenMixin.java
@@ -1,20 +1,15 @@
 package com.erika.vsanalogwarfare.mixin.client;
 
-import net.minecraft.client.gui.components.EditBox;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Pseudo
 @Mixin(targets = "com.forsteri.createendertransmission.transmitUtil.TransmitterScreen", remap = false)
 public class EnderTransmitterScreenMixin {
-    @Shadow private EditBox areaTestInput;
-
-    @Inject(method = "init", at = @At("TAIL"), remap = false)
-    private void vsaw$extendPasswordInput(CallbackInfo callback) {
-        if (areaTestInput != null) areaTestInput.setMaxLength(32);
+    @ModifyConstant(method = "m_7856_()V", constant = @Constant(intValue = 16), remap = false)
+    private int vsaw$extendPasswordInput(int ignored) {
+        return 32;
     }
 }
